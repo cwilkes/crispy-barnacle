535e67ffa4fafb859099e7b20a2d4a2863b8daf2
Chris Wilkes <cwilkes@gmail.com>
2015-09-26 15:51:14 -0700

    remove psycop

--- web/clasher.py
+++ web/clasher.py
@@ -2,11 +2,14 @@ import xmltodict
 import urllib2
 import gzip
 import StringIO
-
+import os
+import boto3
 
 
 class Clasher(object):
     def __init__(self, url):
+        s3 = boto3.resource('s3')
+        #bucket = s3.get_bucket('navishack', validate=False)
         if url.endswith('.gz'):
             response = urllib2.urlopen(url)
             compressed_file = StringIO.StringIO(response.read())

ed1314c24fce2475a6e6220abf989965842394da
Chris Wilkes <cwilkes@gmail.com>
2015-09-26 16:23:59 -0700

    use s3 for storage

--- web/clasher.py
+++ web/clasher.py
@@ -6,14 +6,15 @@ import os
 import boto3
 
 
+S3_BUCKET_NAME = 'navishack'
+
 class Clasher(object):
-    def __init__(self, url):
+    def __init__(self, fn):
         s3 = boto3.resource('s3')
-        #bucket = s3.get_bucket('navishack', validate=False)
-        if url.endswith('.gz'):
-            response = urllib2.urlopen(url)
-            compressed_file = StringIO.StringIO(response.read())
-            decompressed_file = gzip.GzipFile(fileobj=compressed_file)
+        key = s3.Object(S3_BUCKET_NAME, fn)
+        raw_data = StringIO.StringIO(key.get()['Body'].read())
+        if fn.endswith('.gz'):
+            decompressed_file = gzip.GzipFile(fileobj=raw_data)
             self.data = xmltodict.parse(decompressed_file.read())
         else:
-            self.data = xmltodict.parse(urllib2.urlopen(url))
+            self.data = xmltodict.parse(raw_data)

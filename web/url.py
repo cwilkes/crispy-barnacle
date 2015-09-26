adb0de5fe42c84e4c20b1701a0de122651be3ae2
Chris Wilkes <cwilkes@gmail.com>
2015-09-26 12:03:38 -0700

    read in clash file

--- web/url.py
+++ web/url.py
@@ -1,11 +1,34 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, jsonify, redirect
+import os
+import psycopg2
+import urlparse
+import xmltodict
+import urllib2
+import gzip
+import StringIO
+
+
+xml_file = 'https://s3.amazonaws.com/navishack/PC-00-COMP-BBC.xml.gz'
+
+if xml_file.endswith('.gz'):
+    response = urllib2.urlopen(xml_file)
+    compressedFile = StringIO.StringIO(response.read())
+    decompressedFile = gzip.GzipFile(fileobj=compressedFile)
+    j = xmltodict.parse(decompressedFile.read())
+else:
+    j = xmltodict.parse(urllib2.urlopen(xml_file))
+
+
 
 app = Flask(__name__)
+
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
 
 @app.route('/')
 def index():
-    return 'index'
+    return redirect('/clash/')
 
 
 @app.route('/projects/')
@@ -17,6 +40,15 @@ def projects():
 def about():
     return 'The about page'
 
+@app.route('/clash/')
+def clash_index():
+    number_clashes = len(j['exchange']['batchtest']['clashtests']['clashtest'])
+    return render_template('clash_index.html', number_clashes=number_clashes)
+
+@app.route('/clash/<int:number>')
+def clash_by_number(number):
+    clash_info = j['exchange']['batchtest']['clashtests']['clashtest'][number]
+    return jsonify(clash_info)
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
